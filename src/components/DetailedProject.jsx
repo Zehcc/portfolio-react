0b2c993f429e90acb438ad49e1fae1f106541fe6
Tomás Sánchez <Tomassanchezmedina92@gmail.com>
2023-03-12 18:15:01 +0100

    chore: removing ghibli deployment

--- src/components/DetailedProject.jsx
+++ src/components/DetailedProject.jsx
@@ -1,5 +1,5 @@
-import { Link, useParams } from "react-router-dom";
-import { data } from "../Data/Data";
+import { Link, useParams } from 'react-router-dom';
+import { data } from '../Data/Data';
 
 const DetailedProject = () => {
   const { projects } = data;
@@ -19,14 +19,21 @@ const DetailedProject = () => {
       </div>
       <div className='detailed-project-bot'>
         <p>{detailedProject.description}</p>
-        <a
-          className='link-text'
-          href={detailedProject.links[0]}
-          target='_blank'
-          rel='noreferrer'
-        >
-          <i>Desplegar proyecto</i>
-        </a>
+        {detailedProject.links[0] !==
+        'https://ghibli-angular-project.vercel.app/home' ? (
+          <a
+            className='link-text'
+            href={detailedProject.links[0]}
+            target='_blank'
+            rel='noreferrer'
+          >
+            <i>Desplegar proyecto</i>
+          </a>
+        ) : (
+          <a className='link-text' target='_blank' rel='noreferrer'>
+            <i>Despliegue no disponible, han quitado la API pública</i>
+          </a>
+        )}
         <a
           className='link-text'
           href={detailedProject.links[1]}
